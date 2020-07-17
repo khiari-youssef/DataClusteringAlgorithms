7e2ccacbb2bb253caa0d5fb1f6d206b18323db3f
khiari youssef <khiari.youssef98@gmail.com>
2020-07-17 22:24:13 +0100

    added junit dependencies + first test bundle

---
+++ src/test/java/mlMathUtilsTestBundle/AverageDataPointTestCases.java
@@ -0,0 +1,4 @@
+package mlMathUtilsTestBundle;
+
+public class AverageDataPointTestCases {
+}
---
+++ src/test/java/mlMathUtilsTestBundle/MLmathUtilsTestBundle.java
@@ -0,0 +1,15 @@
+package mlMathUtilsTestBundle;
+
+import org.junit.gen5.engine.junit5.JUnit5TestEngine;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@Suite.SuiteClasses({
+EuclidianDistanceTestCases.class,
+AverageDataPointTestCases.class,
+MinDistanceTestCases.class
+})
+public class MLmathUtilsTestBundle {
+
+}
